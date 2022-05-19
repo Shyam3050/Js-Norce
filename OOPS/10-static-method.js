5c2abf6f6ba757da7179380ce24b948a9463538f
shyam <nimmanashyam3050@gmail.com>
2022-05-19 22:42:53 +0530

    U

---
+++ OOPS/10-static-method.js
@@ -0,0 +1,32 @@
+// static for class
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // these methods will added .prototype property
+  calcAGe() {
+    console.log(2022 - this.birthYear);
+  }
+  // this method will directly attached to this class
+  static hey() {
+    console.log("hello world from static method");
+  }
+}
+const jessica = new PersonCl("shyam helo", 2003);
+PersonCl.hey();
+// jessica.hey(); this will not work because its hey() is not a prototype of PersonCl
+
+// add static method to function constructor
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+Person.hey = function () {
+  console.log("this from constructor function hey");
+};
+Person.hey();
+
