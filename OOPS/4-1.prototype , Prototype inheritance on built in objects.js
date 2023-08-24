4f0731cd38a484239588a04ad24df7d4e8be4673
Shyam3050 <nimmanashyam3050@gmail.com>
2023-08-24 13:34:26 +0530

    oops recorection

--- OOPS/4-1.prototype , Prototype inheritance on built in objects.js
+++ OOPS/4-1.prototype , Prototype inheritance on built in objects.js
@@ -12,16 +12,25 @@ Person.prototype.calcAge = function () {
 const jonas = new Person(2, 3);
 jonas.calcAge();
 console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+console.log(jonas.__proto__ === Person.prototype);// true
 console.log(jonas.__proto__ === Person.__proto__); // false
-console.log(Person.prototype === Person.prototype); // false
-// constructor funcion doesn't have the __proto__ them self. __proto__ used to access prototype of instances.
-console.log(Person.prototype.isPrototypeOf(jonas));
-// Person.prototype is not have some property of Person
-console.log(Person.prototype.isPrototypeOf(Person));
+console.log(Person.prototype === Person.prototype); // true
+// constructor funcion  have the __proto__ them self. __proto__ used to access prototype of constructor also.
+
+// Exicution of constructor function
+// 1 new {} empty object is created
+// 2 funtion is called, refer this keyword to newly created obect{}
+// 3 {} object is linked to prototype // it sets the __proto__ property of newly created object with value Person.prototype
+// 4 function automatically return {}; like in above example firstName: "" , birthYear: "" returned.
+
+
+
+console.log(Person.prototype.isPrototypeOf(jonas));// true
+console.log(Person.prototype.isPrototypeOf(Person));// false
+// Person.prototype is not the property of Person
 Person.prototype.species = "Homo Sapiens";
 console.log(jonas.species);
-console.log(jonas.hasOwnProperty("species"));
+console.log(jonas.hasOwnProperty("species")); // false
 
 // ///////////////////////////
 // //////////////////////////
--- promises-AkshaySaini/script.js
+++ promises-AkshaySaini/script.js
@@ -1,16 +1,40 @@
-const cart = ["pant", "kurta","shirt","lungi"]
+// EPISODE -1
+//{ const GITHUB_API = "https://api.github.com/users/Shyam3050"
 
-// createOrder(cart); // return orderId
+// const user = fetch(GITHUB_API)
+// // console.log(user)
 
-// proceedTopayment(orderId)
+// user.then((data) => data.json()).then(data => console.log(data))}
 
-function x (fn){
-    // fn();
-    return fn;
-}
- const y = x(function (){
+const cart = ["pant", "kurta", "shirt", "lungi"];
+// const cart = []
 
-    console.log("anonymous rendered")
-    return 1;
-})
-console.log(y())
+// EPISODE - 2
+
+const createOrder = (cart) => {
+  const pr = new Promise((resolve, reject) => {
+    if (!cart.length) {
+      const err = new Error("cart is empty");
+      reject(err);
+    }
+    const orderid = "jsdkf849749kd843jg9";
+    setTimeout(() => {
+      resolve(orderid);
+    }, 5000);
+  });
+  return pr;
+};
+
+const proceedToPayment = (orderid) => {
+  return new Promise((resolve, reject) =>
+    resolve(`${orderid} payment successfull`)
+  );
+};
+createOrder(cart)
+  .then((orderid) => {
+    console.log(orderid);
+    return orderid;
+  })
+  .then((orderId) => proceedToPayment(orderId))
+  .then((paymentInfo) => console.log(paymentInfo))
+  .catch((err) => console.log(err));
