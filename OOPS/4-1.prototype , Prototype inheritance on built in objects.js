dcb101e332b456e1bd1ef89f80f47a6c12c17b78
Shyam3050 <nimmanashyam3050@gmail.com>
2023-08-26 11:49:13 +0530

    inheritence built in object.

--- OOPS/4-1.prototype , Prototype inheritance on built in objects.js
+++ OOPS/4-1.prototype , Prototype inheritance on built in objects.js
@@ -1,6 +1,6 @@
 // Each and every function in javascript have automatically property called prototype that includes also constructor function.
-// Every object is created by constructor function will get access to all the methods and property that we define on constructor prototype property. 
-// 
+// Every object is created by constructor function will get access to all the methods and property that we define on constructor prototype property.
+//
 
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
@@ -12,7 +12,7 @@ Person.prototype.calcAge = function () {
 const jonas = new Person(2, 3);
 jonas.calcAge();
 console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);// true
+console.log(jonas.__proto__ === Person.prototype); // true
 console.log(jonas.__proto__ === Person.__proto__); // false
 console.log(Person.prototype === Person.prototype); // true
 // constructor funcion  have the __proto__ them self. __proto__ used to access prototype of constructor also.
@@ -23,10 +23,8 @@ console.log(Person.prototype === Person.prototype); // true
 // 3 {} object is linked to prototype // it sets the __proto__ property of newly created object with value Person.prototype
 // 4 function automatically return {}; like in above example firstName: "" , birthYear: "" returned.
 
-
-
-console.log(Person.prototype.isPrototypeOf(jonas));// true
-console.log(Person.prototype.isPrototypeOf(Person));// false
+console.log(Person.prototype.isPrototypeOf(jonas)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
 // Person.prototype is not the property of Person
 Person.prototype.species = "Homo Sapiens";
 console.log(jonas.species);
@@ -36,6 +34,7 @@ console.log(jonas.hasOwnProperty("species")); // false
 // //////////////////////////
 
 // prototype inheritance on built in object
+
 // const Person = function(firstName){
 //   this.firstName = firstName
 // }
@@ -47,3 +46,9 @@ console.log(jonas.hasOwnProperty("species")); // false
 // console.log(arr.__proto__ === Array.prototype);
 // console.log(arr.__proto__.__proto__ === Object.prototype);
 
+const arr = [1, 2, 4, 3, 2, 3, 1, 4, 5, 4, 5];
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
